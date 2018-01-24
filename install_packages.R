390435c3175a3fae02842961ed379be78ff35d45
RaghavTamhankar <31287687+RaghavTamhankar@users.noreply.github.com>
2018-01-24 09:38:26 -0500

    Create install_packages.R

---
+++ install_packages.R
@@ -0,0 +1,3 @@
+install.packages(c("caret","kernlab","e1071","MASS","sqldf","mice","gmodels","pROC","RSNNS","leaps","data.table","tree","reshape2","boot","tidyr","kohonen","dplyr","RColorBrwer"),
+                 repos = "http://lib.stat.cmu.edu/R/CRAN", 
+                 dependencies = TRUE)

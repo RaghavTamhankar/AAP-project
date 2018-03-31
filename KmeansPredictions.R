a809e45b5275942e68b4f1bbcc156bb2317855e5
Raghav Tamhankar <tamhankarraghav@gmail.com>
2018-03-31 11:01:08 -0400

    Delete KmeansPredictions.R

--- KmeansPredictions.R
+++
@@ -1,7 +0,0 @@
-
-## Kmeans clustering code here (data inputted here is cleaned and preprocessed)
-## complete all the regressions here
-
-
-
-## at the end, output the file with cluster numbers for each row 
